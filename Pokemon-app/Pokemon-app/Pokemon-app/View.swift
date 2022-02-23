ebe969925179defa8ab45a541f5d203a880d51ff
Ermakov Alexander <alexandr.ermakov@idapgroup.com>
2022-02-23 08:58:37 +0200

    Changed

--- Pokemon-app/Pokemon-app/Pokemon-app/View.swift
+++ Pokemon-app/Pokemon-app/Pokemon-app/View.swift
@@ -7,35 +7,36 @@
 
 import UIKit
 
-class View: UIView, UITableViewDelegate, UITableViewDataSource {
+class View: UIView {
     
     // MARK: -
     // MARK: Public variables
     
-    @IBOutlet var pokemonsNames: UITableView?
+    var pokemonsNames = UITableView()
     public var numberOfNames: Int?
     public var namesArray = ["one", "two", "three"]
     
     // MARK: -
     // MARK: Public functions
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return numberOfNames ?? 0
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = pokemonsNames?.dequeueReusableCell(withIdentifier: "CellIdentifier", for: indexPath)
-        cell?.textLabel?.text = namesArray[indexPath.row]
-        return cell!
-    }
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return namesArray.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = pokemonsNames.dequeueReusableCell(withIdentifier: "CellIdentifier", for: indexPath)
+//        cell.textLabel?.text = namesArray[indexPath.row]
+//        return cell
+//    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
     }
     
     func printList( _ listOfNames: Result<[String], Error>) -> () {
-        self.pokemonsNames?.delegate = self
-        self.pokemonsNames?.dataSource = self
+        self.pokemonsNames = UITableView(frame: CGRect(x: 0, y: 0, width: 100, height: 100), style: .plain)
+        self.pokemonsNames.delegate = window?.rootViewController
+        self.pokemonsNames.dataSource = window?.rootViewController
         
         switch listOfNames {
         case .success(let names):
--- Pokemon-app/Pokemon-app/Pokemon-app/ViewController.swift
+++ Pokemon-app/Pokemon-app/Pokemon-app/ViewController.swift
@@ -22,7 +22,7 @@ extension RootViewGettable {
     }
 }
 
-class ViewController: UIViewController, RootViewGettable {
+class ViewController: UIViewController, RootViewGettable, UITableViewDelegate, UITableViewDataSource {
     
     typealias RootView = View
     
@@ -59,5 +59,15 @@ class ViewController: UIViewController, RootViewGettable {
             self?.printData(data: data)
         }
         self.requester.pokemonsNames(limit: 10)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellIdentifier", for: indexPath)
+        cell.textLabel?.text = "<!>"
+        return cell
     }
 }
