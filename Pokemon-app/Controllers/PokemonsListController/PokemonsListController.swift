631e1e28408d22a9939be91b68e1c2f25dee400c
Bogdan Userman Kikacheishvili <bogdan.kikacheishvili@idapgroup.com>
2022-05-12 11:05:54 +0300

    Fixing merger conflict.

---
+++ Pokemon-app/Controllers/PokemonsListController/PokemonsListController.swift
@@ -0,0 +1,62 @@
+import UIKit
+import RxSwift
+
+class PokemonsListController: BaseViewController<PokemonsListView> {
+    
+    // MARK: -
+    // MARK: Variables
+    
+    let provider: PokemonsDataProvider
+    weak var pokemonsListDelegate: PokemonsListDelegate?
+    var pokemons = [Pokemon]()
+    
+    // MARK: -
+    // MARK: Initializators
+    
+    init(provider: PokemonsDataProvider){
+        self.provider = provider
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: -
+    // MARK: Public functions
+    
+    public func sendToPrint(data: [Pokemon]) {
+        DispatchQueue.main.async {
+            self.pokemons = data
+            self.rootView?.tableView?.reloadData()
+        }
+    }
+    
+    func pokemonsNames() {
+        self.provider.list(count: 20) { [weak self] response in
+            switch response {
+            case .success(let data):
+                self?.sendToPrint(data: data)
+            case .failure(_):
+                print("Incorrect response from server")
+            }
+        }
+    }
+    
+    func processPokemon(cellNumber: Int) {
+        let detailsURL = self.pokemons[cellNumber].url
+        
+        self.events.onNext(.showDetails(url: detailsURL))
+    }
+    
+    // MARK: -
+    // MARK: ViewController Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "List of Pokemons"
+        self.pokemonsNames()
+    }
+}
